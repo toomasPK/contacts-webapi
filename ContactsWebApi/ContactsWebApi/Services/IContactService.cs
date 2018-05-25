3bdc34f62f134f9a309d130092333869011e6a7f
toomasPK <toomas.karvinen@gmail.com>
2018-05-25 09:11:05 +0300

    Added Contact Service

--- ContactsWebApi/ContactsWebApi/Services/IContactService.cs
+++ ContactsWebApi/ContactsWebApi/Services/IContactService.cs
@@ -11,5 +11,8 @@ namespace ContactsWebApi.Services
         List<Contact> GetContacts();
         Contact GetContactById(int id);
         Contact GetContactByID(int id);
+        Contact CreateContact(Contact contact);
+        Contact UpdateContact(int id,Contact contact);
+        void DeleteContact(int id);
     }
 }
